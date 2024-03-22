a194fb4f0fa48dcdedac76b2cdfef2ff68533f2d
John-Franklin <johnfranklin42@gmail.com>
2024-03-21 23:31:49 -0700

    AAA

--- go.mod
+++ go.mod
@@ -25,7 +25,11 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,82 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type postgres struct {
+	db *pgxpool.Pool
+}
+
+var (
+	pgInstance *postgres
+	pgError    error
+	pgOnce     sync.Once
+)
+
+func NewPG(ctx context.Context, connString string) (*postgres, error) {
+	pgOnce.Do(func() {
+		db, err := pgxpool.New(ctx, connString)
+		if err != nil {
+			pgInstance = nil
+			pgError = fmt.Errorf("unable to create connection pool: %w", err)
+		} else {
+			pgInstance = &postgres{db}
+			pgError = nil
+		}
+	})
+	return pgInstance, pgError
+}
+
+const databaseUrl = "postgres://postgres:password@localhost:5432/postgres"
+
+func getPG() (*postgres, error) {
+	databaseUrl := "postgres://postgres:password@localhost:5432/postgres"
+	return NewPG(context.Background(), databaseUrl)
+}
+func (pg *postgres) Ping(ctx context.Context) error {
+	return pg.db.Ping(ctx)
+}
+
+func (pg *postgres) Close() {
+	pg.db.Close()
+}
 
 // votes slice to seed record vote data.
 var votes = []vote{
-	{CampaignId: "1", UserId: "1", UnionId: "1", Approve: false},
+	{CampaignId: 1, UserId: 1, UnionId: 1, Approve: false},
 }
 
 // vote represents data about a record vote.
 type vote struct {
-	ID         string `json:"id"`
-	CampaignId string `json:"campaign_id"`
-	UserId     string `json:"user_id"`
-	UnionId    string `json:"union_id"`
-	Approve    bool   `json:"approve"`
+	ID         int  `json:"id"`
+	CampaignId int  `json:"campaign_id"`
+	UserId     int  `json:"user_id"`
+	UnionId    int  `json:"union_id"`
+	Approve    bool `json:"approve"`
 }
 
-// postvotes adds an vote from JSON received in the request body.
-func postvotes(c *gin.Context) {
+var (
+	voteTableOnce sync.Once
+)
+
+func beforeVoteAccess() {
+	voteTableOnce.Do(func() {
+		pg, _ := getPG()
+		pg.db.Query(context.Background(), "CREATE TABLE votes (CampaignId: int, UserId: int, UnionId: int, Approve: bit)")
+	})
+}
+
+// postVotes adds an vote from JSON received in the request body.
+func postVotes(c *gin.Context) {
+	// This should probably be middleware but let's not for now
+	beforeVoteAccess()
 	var newvote vote
 
 	// Call BindJSON to bind the received JSON to
@@ -30,21 +85,22 @@ func postvotes(c *gin.Context) {
 		return
 	}
 	// library to consider: jet for SQL?
+	// pgx is appropriate for the demo in question.
 	// Add the new vote to the slice.
 	votes = append(votes, newvote)
 	c.IndentedJSON(http.StatusCreated, newvote)
 }
 
-// getvotes responds with the list of all votes as JSON.
-func getvotes(c *gin.Context) {
-	return pgx.
-		c.IndentedJSON(http.StatusOK, votes)
+// getVotes responds with the list of all votes as JSON.
+func getVotes(c *gin.Context) {
+	// This should probably be middleware but let's not for now
+	c.IndentedJSON(http.StatusOK, votes)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/votes", getvotes)
-	router.POST("/votes", postvotes)
+	router.GET("/votes", getVotes)
+	router.POST("/votes", postVotes)
 	router.Run("localhost:8080")
 	// router.RunTLS(":8080", "server.pem", "server.key")
 }
