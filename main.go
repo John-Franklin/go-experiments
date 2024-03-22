ee2bd1040acc64cf9cdd9aa45ef1811b87dfc10b
John-Franklin <johnfranklin42@gmail.com>
2024-03-21 18:55:41 -0700

    work in progress

--- main.go
+++ main.go
@@ -1,43 +1,50 @@
 package main
 
-import "net/http"
-import "github.com/gin-gonic/gin"
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
-// postAlbums adds an album from JSON received in the request body.
-func postAlbums(c *gin.Context) {
-    var newAlbum album
+import (
+	"net/http"
 
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
-    if err := c.BindJSON(&newAlbum); err != nil {
-        return
-    }
-	// library to consider: jet for SQL?
-    // Add the new album to the slice.
-    albums = append(albums, newAlbum)
-    c.IndentedJSON(http.StatusCreated, newAlbum)
+	"github.com/gin-gonic/gin"
+)
+
+// votes slice to seed record vote data.
+var votes = []vote{
+	{CampaignId: "1", UserId: "1", UnionId: "1", Approve: false},
 }
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
+
+// vote represents data about a record vote.
+type vote struct {
+	ID         string `json:"id"`
+	CampaignId string `json:"campaign_id"`
+	UserId     string `json:"user_id"`
+	UnionId    string `json:"union_id"`
+	Approve    bool   `json:"approve"`
+}
+
+// postvotes adds an vote from JSON received in the request body.
+func postvotes(c *gin.Context) {
+	var newvote vote
+
+	// Call BindJSON to bind the received JSON to
+	// newvote.
+	if err := c.BindJSON(&newvote); err != nil {
+		return
+	}
+	// library to consider: jet for SQL?
+	// Add the new vote to the slice.
+	votes = append(votes, newvote)
+	c.IndentedJSON(http.StatusCreated, newvote)
+}
+
+// getvotes responds with the list of all votes as JSON.
+func getvotes(c *gin.Context) {
+	return pgx.
+		c.IndentedJSON(http.StatusOK, votes)
 }
 
 func main() {
-    router := gin.Default()
-    router.GET("/albums", getAlbums)
-    router.POST("/albums", postAlbums)
-    router.Run("localhost:8080")
+	router := gin.Default()
+	router.GET("/votes", getvotes)
+	router.POST("/votes", postvotes)
+	router.Run("localhost:8080")
 	// router.RunTLS(":8080", "server.pem", "server.key")
 }
